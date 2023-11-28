628d4ca881b95d9b2d748f7bd4bac36fc64d6429
chanderlud <me@chanchan.dev>
2023-11-27 21:36:36 -0800

    changed metadata loading for efficiency

--- src/receiver/metadata.rs
+++ src/receiver/metadata.rs
@@ -1,8 +1,9 @@
+use std::io::SeekFrom;
 use std::path::{Path, PathBuf};
 
 use tokio::fs::{remove_file, File, OpenOptions};
 use tokio::io;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
 
 pub(crate) struct Metadata {
     pub(crate) initial_index: u64, // the initial index
@@ -43,26 +44,34 @@ impl Metadata {
 
     /// load metadata from file
     async fn load(file_path: &Path) -> io::Result<Self> {
+        // format the path to the metadata file
         let file_path = Self::format_path(file_path);
 
-        // open file for reading and writing
+        // open the file for reading and writing
         let mut file = OpenOptions::new()
             .read(true)
             .write(true)
             .open(&file_path)
             .await?;
 
-        let mut buf = [0; 8]; // create buffer for data
+        // seek to the end of the file
+        let len = file.seek(SeekFrom::End(0)).await?;
 
-        // read data into buffer until EOF
-        while let Ok(read) = file.read_exact(&mut buf).await {
-            if read == 0 {
-                break; // the buf now contains the last index
-            }
-        }
+        // if the file is not empty, seek back 8 bytes from the end and read the last index
+        let index = if len > 0 {
+            let mut buf = [0; 8]; // create buffer for data
 
+            file.seek(SeekFrom::End(-8)).await?;
+            file.read_exact(&mut buf).await?;
+
+            u64::from_be_bytes(buf)
+        } else {
+            0
+        };
+
+        // return a new instance of the Metadata struct
         Ok(Self {
-            initial_index: u64::from_be_bytes(buf),
+            initial_index: index,
             writer: file,
             file_path,
         })
