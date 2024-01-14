d6aafa68ccbf779ffe18f857f0b12ecd93e8a339
chanderlud <me@chanchan.dev>
2024-01-14 10:15:38 -0800

    macOs changes, protobuf optimization

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     #[cfg(unix)]
-    std::env::set_var("PROTOC", protobuf_src::protoc());
+    std::env::set_var("PROTOC", "/bin/protoc");
     prost_build::compile_protos(&["src/items.proto"], &["src/"])?;
 
     Ok(())
---
+++ ci/pre_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the URL and file name
+URL="https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip"
+ZIP_FILE="protoc-25.2-linux-x86_64.zip"
+BIN_FOLDER="bin"
+
+# Download the ZIP file
+wget $URL
+
+# Extract the ZIP file
+unzip $ZIP_FILE
+
+# Move a file from the bin folder to /bin
+mv $BIN_FOLDER/protoc /bin/
