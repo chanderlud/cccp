729430693e504e41edb9d5efe0a2a76f289a6bf7
chanderlud <me@chanchan.dev>
2024-01-14 14:50:10 -0800

    try to get protobuf setup right

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     #[cfg(unix)]
-    std::env::set_var("PROTOC", "/bin/protoc");
+    std::env::set_var("PROTOC", "/usr/local/bin/protoc");
     prost_build::compile_protos(&["src/items.proto"], &["src/"])?;
 
     Ok(())
