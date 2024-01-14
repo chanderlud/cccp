8c1b266da31f7e5a614a266b00e558a8db191fd8
chanderlud <me@chanchan.dev>
2024-01-13 21:29:38 -0800

    trying protobuf-src on unix

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
+    #[cfg(unix)]
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     prost_build::compile_protos(&["src/items.proto"], &["src/"])?;
 
     Ok(())
