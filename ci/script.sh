fa5f9ffc7bea769f91a39f7741a28d3f782606ac
chanderlud <me@chanchan.dev>
2024-01-14 16:34:57 -0800

    mips target fixes

--- ci/script.sh
+++ ci/script.sh
@@ -9,20 +9,32 @@ main() {
         # mips targets require opt-level 1 due to rust-lang/rust/issues/108835
         mips-unknown-linux-musl)
             RUSTFLAGS='-C opt-level=1' cross build --target $TARGET
+
+            if [ ! -z $DISABLE_TESTS ]; then
+                return
+            fi
+
+            RUSTFLAGS='-C opt-level=1' cross clippy --target $TARGET
             ;;
         mipsel-unknown-linux-musl)
             RUSTFLAGS='-C opt-level=1' cross build --target $TARGET
+
+            if [ ! -z $DISABLE_TESTS ]; then
+                return
+            fi
+
+            RUSTFLAGS='-C opt-level=1' cross clippy --target $TARGET
             ;;
         *)
             cross build --target $TARGET
+
+            if [ ! -z $DISABLE_TESTS ]; then
+                return
+            fi
+
+            cross clippy --target $TARGET
             ;;
     esac
-
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
-    fi
-
-    cross clippy --target $TARGET
 }
 
 # we don't run the "test phase" when doing deploys
