19283660f88f96ab5e752cbe83cf7ca1c9135558
chanderlud <me@chanchan.dev>
2024-01-15 11:43:18 -0800

    macos almost working

--- ci/script.sh
+++ ci/script.sh
@@ -7,12 +7,12 @@ main() {
 
     if [ "$TARGET" = "mips-unknown-linux-musl" ] || [ "$TARGET" = "mipsel-unknown-linux-musl" ]; then
         # MIPS targets require opt-level 1 due to a known issue in Rust
-        RUSTFLAGS='-C opt-level=1'
-        cross build --target $TARGET
+
+        RUSTFLAGS='-C opt-level=1' cross build --target $TARGET
 
         [ -n "${DISABLE_TESTS:-}" ] && return
 
-        cross clippy --target $TARGET
+        RUSTFLAGS='-C opt-level=1' cross clippy --target $TARGET
     else
         cross build --target $TARGET
 
