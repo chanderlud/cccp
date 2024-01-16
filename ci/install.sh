37206535f8d20abbf02c77ee1faaec1e109a7e6a
chanderlud <me@chanchan.dev>
2024-01-15 17:02:04 -0800

    macos almost working

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,10 @@
 set -ex
 
 main() {
-    # Install binstall
+    # install binstall
     curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-    # Install cross w/ binstall
+    # install cross w/ binstall
     cargo binstall cross --git https://github.com/cross-rs/cross --no-confirm
 
     # macos targets require building a custom image
@@ -15,18 +15,17 @@ main() {
             git submodule update --init --remote
 
             case "$TARGET" in
-                x86_64-apple-darwin)
-                    MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz'
-                    ;;
-                aarch64-apple-darwin)
-                    MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz'
-                    ;;
                 i686-apple-darwin)
-                    MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz'
-                    ;;
+                  # sdk v10.13 for i686
+                  MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz'
+                  ;;
+                *)
+                  # sdk v11.3 for x86_64 & aarch64
+                  MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz'
+                  ;;
             esac
 
-            cargo build-docker-image $TARGET-cross --tag local --build-arg MACOS_SDK_URL
+            cargo build-docker-image $TARGET-cross --tag local --build-arg $MACOS_SDK_URL
             ;;
     esac
 }
--- ci/script.sh
+++ ci/script.sh
@@ -10,19 +10,10 @@ main() {
         $HOME/.cargo/bin/rustup component add rust-src clippy
 
         cross build --target $TARGET -Z build-std
-
-        # [ -n "${DISABLE_TESTS:-}" ] && return
-
-        # cross clippy --target $TARGET -Z build-std
         ;;
       "mips-unknown-linux-musl" | "mipsel-unknown-linux-musl")
         # MIPS targets require opt-level 1 due to a known issue in Rust
-
         RUSTFLAGS='-C opt-level=1' cross build --target $TARGET
-
-        [ -n "${DISABLE_TESTS:-}" ] && return
-
-        RUSTFLAGS='-C opt-level=1' cross clippy --target $TARGET
         ;;
       *)
         cross build --target $TARGET
