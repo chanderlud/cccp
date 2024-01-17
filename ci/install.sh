e4b17025d8f3a80d69a26d755df8a4fd92ab5367
chanderlud <me@chanchan.dev>
2024-01-17 14:56:19 -0800

    deploy not working quite right

--- ci/install.sh
+++ ci/install.sh
@@ -12,28 +12,38 @@ main() {
     # these targets require building a custom image
     case "$TARGET" in
         *darwin*)
-            git clone https://github.com/cross-rs/cross
-            cd cross
-            git submodule update --init --remote
+            IMAGE_NAME="$TARGET-cross:local"
+            if ! docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+                git clone https://github.com/cross-rs/cross
+                cd cross
+                git submodule update --init --remote
 
-            case "$TARGET" in
-                i686-apple-darwin)
-                    # sdk v10.13 for i686
-                    MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz'
-                    ;;
-                *)
-                    # sdk v11.3 for x86_64 & aarch64
-                    MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz'
-                    ;;
-            esac
+                case "$TARGET" in
+                    i686-apple-darwin)
+                        # sdk v10.13 for i686
+                        MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz'
+                        ;;
+                    *)
+                        # sdk v11.3 for x86_64 & aarch64
+                        MACOS_SDK_URL='MACOS_SDK_URL=https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz'
+                        ;;
+                esac
 
-            cargo build-docker-image $TARGET-cross --tag local --build-arg $MACOS_SDK_URL --cache-from=$TARGET-cross:local
+                cargo build-docker-image $TARGET-cross --tag local --build-arg $MACOS_SDK_URL
+            else
+                echo "Docker image $IMAGE_NAME already exists."
+            fi
             ;;
         "aarch64-pc-windows-msvc" | "x86_64-pc-windows-msvc" | "i686-pc-windows-msvc")
-            git clone https://github.com/cross-rs/cross
-            cd cross
-            git submodule update --init --remote
-            cargo build-docker-image $TARGET-cross --tag local --cache-from=$TARGET-cross:local
+            IMAGE_NAME="$TARGET-cross:local"
+            if ! docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+                git clone https://github.com/cross-rs/cross
+                cd cross
+                git submodule update --init --remote
+                cargo build-docker-image $TARGET-cross --tag local
+            else
+                echo "Docker image $IMAGE_NAME already exists."
+            fi
             ;;
     esac
 }
