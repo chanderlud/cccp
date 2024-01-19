dc4639b6654c27c17c108968c051a78df80cef20
chanderlud <me@chanchan.dev>
2024-01-18 18:09:21 -0800

    CI changes

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -3,33 +3,30 @@
 set -euxo pipefail
 
 main() {
-    local src=$(pwd)
-    stage=$(mktemp -d)
+    local SRC=$(pwd)
+    STAGE=$(mktemp -d)
 
     test -f Cargo.lock || cargo generate-lockfile
 
     case $TARGET in
         *darwin*)
             $HOME/.cargo/bin/rustup component add rust-src
-
-            cross rustc --target $TARGET --release -- -C lto
-            cp target/$TARGET/release/cccp $stage/
-            ;;
-        *windows*)
-            cross rustc --target $TARGET --release -- -C lto
-            cp target/$TARGET/release/cccp.exe $stage/
-            ;;
-        *)
-            cross rustc --target $TARGET --release -- -C lto
-            cp target/$TARGET/release/cccp $stage/
             ;;
     esac
 
-    cd $stage
+    cross build --target $TARGET --release
+
+    if [[ -f "target/${TARGET}/release/cccp.exe" ]]; then
+        mv "target/${TARGET}/release/cccp.exe" "${STAGE}/"
+    else
+        mv "target/${TARGET}/release/cccp" "${STAGE}/"
+    fi
+
+    cd $STAGE
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
     cd $src
 
-    rm -rf $stage
+    rm -rf $STAGE
 }
 
 main
